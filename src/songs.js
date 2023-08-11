15a124db75c40f2fbdba1af23d32822521bc30a8
Wilkins Morales <wilkinscom@gmail.com>
2023-08-11 17:16:54 +0200

    create mockup songs file

---
+++ src/songs.js
@@ -0,0 +1,34 @@
+const songs = [
+  {
+    id: 1,
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    album: "÷ (Deluxe)",
+  },
+  {
+    id: 2,
+    name: "Blinding Lights",
+    artist: "The Weeknd",
+    album: "After Hours",
+  },
+  {
+    id: 3,
+    name: "Stairway to Heaven",
+    artist: "Led Zeppelin",
+    album: "Led Zeppelin IV",
+  },
+  {
+    id: 4,
+    name: "Bohemian Rhapsody",
+    artist: "Queen",
+    album: "A Night at the Opera",
+  },
+  {
+    id: 5,
+    name: "Hotel California",
+    artist: "Eagles",
+    album: "Hotel California",
+  },
+];
+
+export default songs;
